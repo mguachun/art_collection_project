47c921892ae2a93b1c0c1f3d1e3dcfaf59e76e3e
Melissa Guachun <1212malka@gmail.com>
2021-04-21 11:11:50 -0400

    created seeds

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Collection.find_or_create_by(artist: "Vincent van Gogh", title: "Starry Night", year: 1889, movement: "Post Impressionism",location:"France" , medium: "oil")
+Collection.find_or_create_by(artist: "Piet Mondrian", title: "Broadway Boogie-Woogie", year: 1943, movement: "De Stijl",location:"France" , medium: "oil")
+Collection.find_or_create_by(artist: "Käthe Kollwitz", title: "The Mothers from War", year: 1922, movement: "German Expressionism",location:"Germany" , medium: "woodcut")
+Collection.find_or_create_by(artist: "Hieronymous Bosch", title: "The Garden of Earthly Delights", year: 1515, movement: "Northern Renaissance",location:"Spain" , medium: "oil")
