4e640f237e3e1c63c7123ab0387093cdab77f942
Melissa Guachun <1212malka@gmail.com>
2021-04-29 12:47:49 -0400

    deleted find_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,10 +34,6 @@ class ApplicationController < Sinatra::Base
       User.find_by_id(session[:user_id])
     end
 
-    def find_user
-      @user = User.find_by_email(params[:email])
-    end
-
     def redirect_if_logged_in
       if logged_in?
         redirect "/"
